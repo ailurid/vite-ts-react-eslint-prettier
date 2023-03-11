d2bc3f491e42cf532918c296afb584edf3e60c14
ailurid <23497994+ailurid@users.noreply.github.com>
2023-03-11 03:27:54 +0000

    better

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: { hmr: { clientPort: process.env.CODESPACES ? 443 : undefined } }
+  plugins: [react()]
 });
